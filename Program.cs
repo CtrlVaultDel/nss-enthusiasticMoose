6890dfccb51328aaad51ce8d0a901ccaf06ce8bf
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-02-26 13:05:05 -0600

    Phase 4 Finished

--- Program.cs
+++ Program.cs
@@ -3,11 +3,14 @@
 Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.Write("--------------------------------------------");
 Console.WriteLine();
-MooseSays();
 
-void MooseSays()
+// What the moose will say
+MooseSays("H I, I'M  E N T H U S I A S T I C !");
+MooseSays("I really am enthusiastic");
+
+void MooseSays(string message)
 {
-    Console.WriteLine(@"
+    Console.WriteLine($@"
                                       _.--^^^--,
                                     .'          `\
   .-^^^^^^-.                      .'              |
@@ -24,7 +27,7 @@ void MooseSays()
                '-'    | /       \ |
                       |  .-. .-.  |
                       \ / o| |o \ /
-                       |   / \   |    H I, I'M  E N T H U S I A S T I C !
+                       |   / \   |    {message}
                       / `^`   `^` \
                      /             \
                     | '._.'         \
